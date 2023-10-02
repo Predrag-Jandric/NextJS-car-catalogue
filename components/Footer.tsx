577d3e647fd79ca25391477f26b2f990c206b961
predrag jandric <predrag.jandric.bg@gmail.com>
2023-10-02 23:23:51 +0200

    working on FOOTER, wrote all the data for maps

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div>
-      footer
-    </div>
+    <footer className='flex flex-col text-black-100 mt-5 border-t border-gray-100'>
+      <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
+        <div className='flex flex-col justify-start items-start gap-6'>
+        <Image src="/logo.svg" alt='car hub logo' width={118} height={18} className='object-contain' />
+        <p className='text-base text-gray-700'>
+            CarHub 2023 <br />
+            All right reserved &copy;
+        </p>
+        </div>
+        <div className='footer__links'>
+
+        </div>
+      </div>
+    </footer>
   )
 }
 
