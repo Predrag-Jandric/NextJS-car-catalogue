dd4b1c53127c371cc19b9da65977a644eddb77c7
predrag jandric <predrag.jandric.bg@gmail.com>
2023-10-02 23:06:46 +0200

    NAVBAR & HERO complete, working on FOOTER

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,16 +2,17 @@
 
 import React from 'react'
 import Image from 'next/image'
+import { CustomButtonProps } from "@/types"
 
-const CustomButton = () => {
+const CustomButton = ({title, containerStyles, handleClick, btnType}: CustomButtonProps) => {
   return (
     <button
     disabled={false}
-    type={"button"}
-    className={`custom-btn`}
-    onClick={() => {}}
+    type={btnType || "button"}
+    className={`custom-btn ${containerStyles}`}
+    onClick={handleClick}
     >
-        <span className={`flex-1`}>title</span>
+        <span className={`flex-1`}>{title}</span>
     </button>
   )
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,15 @@
+"use client"
+
 import React from 'react'
 import Image from 'next/image'
 import { CustomButton } from '.'
 
 const Hero = () => {
+
+    const handleScroll = () => {
+
+    }
+
   return (
     <div className='hero' >
       <div className="flex-1 pt-36 padding-x">
@@ -15,8 +22,16 @@ const Hero = () => {
 
         <CustomButton 
         title="Explore Cars"
+        btnType="button"
         containerStyles="bg-primary-blue text-white rounded-full mt-10"
+        handleClick={handleScroll}
         />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src={"/hero.png"} alt='hero' fill className='object-contain' />
+          <div className='hero__image-overlay' />
+        </div>
       </div>
     </div>
   )
