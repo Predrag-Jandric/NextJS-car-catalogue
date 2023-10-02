5d52a41494eadd849819408eed45eb1827dd82f9
predrag jandric <predrag.jandric.bg@gmail.com>
2023-10-02 23:56:17 +0200

    working on search form, saving work

---
+++ components/SearchBar.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import React from 'react'
+import { SearchManufacturer } from '.'
+import {useState} from "react"
+
+const SearchBar = () => {
+    const [manufacturer, setManufacturer] = useState('')
+
+
+    const handleSearch = () => {}
+
+  return (
+    <form className='searchbar' onSubmit={handleSearch}>
+        <div className="searchbar__item">
+            <SearchManufacturer
+            setManufacturer={setManufacturer}
+            />
+        </div>
+    </form>
+  )
+}
+
+export default SearchBar
+
--- types/index.ts
+++ types/index.ts
@@ -8,3 +8,8 @@ export interface CustomButtonProps {
     MouseEventHandler<HTMLButtonElement>
     btnType: "button" | "submit"
 }
+
+export interface SearchManufacturerProps {
+    manufacturer: string
+    setManufacturer: (manufacturer: string) => void
+}
