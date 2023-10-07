188daad91c60ac3a9d45e1d783252566ee379234
predrag jandric <predrag.jandric.bg@gmail.com>
2023-10-07 22:40:15 +0200

    updating SearchBar.jsx

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,23 +1,59 @@
 "use client"
 
+import Image from "next/image"
 import { SearchManufacturer } from "./"
 import { useState } from "react"
 
+// component only to be used withing THIS component
+const SearchButton = ({otherClasses}: {otherClasses: string}) => (
+  <button 
+  type="submit" 
+  className={`-ml-3 z-10 ${otherClasses}`}
+  >
+    <Image
+      src={"/magnifying-glass.svg"}
+      alt={"magnifying glass"}
+      width={40}
+      height={40}
+      className='object-contain'
+    />
+  </button>
+)
 
 const SearchBar = () => {
     const [manufacturer, setManufacturer] = useState('')
-
+    const [model, setModel] = useState("")
 
     const handleSearch = () => {}
 
   return (
     <form className='searchbar' onSubmit={handleSearch}>
-        <div className="searchbar__item">
-            <SearchManufacturer
-            manufacturer={manufacturer}
-            setManufacturer={setManufacturer} 
-          />
-        </div>
+         <div className='searchbar__item'>
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer} 
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <div className='searchbar__item'>
+        <Image
+          src='/model-icon.png'
+          width={25}
+          height={25}
+          className='absolute w-[20px] h-[20px] ml-4'
+          alt='car model'
+        />
+        <input
+          type='text'
+          name='model'
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder='Tiguan...'
+          className='searchbar__input'
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <SearchButton otherClasses='max-sm:hidden' />
     </form>
   )
 }
