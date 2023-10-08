22ee8e149b89779bca49aedec54ba5a67ab5ff17
predrag jandric <predrag.jandric.bg@gmail.com>
2023-10-08 23:04:18 +0200

    updated readme.md to template, content, live links and showcase to be added in the project end

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# IN PROGRESS
+
+# NextJS - E Commerce car shop
+
+# 🔗 [Live Preview]()
+![Design preview]()
+
+
+---
+## About 👋
+
+[text]
+
+---
+
+## Features 👨‍💻
+
+* [text]
+
+* [text]
+
+---
+
+## Languages
+
+* [text]
+* [text]
+
--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -20,7 +20,7 @@ const CustomFilter = ({title, options}: CustomFilterProps) => {
           {/* Button for the listbox */}
           <Listbox.Button className='custom-filter__btn'>
             <span className='block truncate'>{selected.title}</span>
-            <Image src='/chevron-up-down.svg' width={20} height={20} className='ml-4 object-contain' alt='chevron_up-down' />
+            <Image src='/chevron-up-down.svg' width={20} height={20} className='ml-4 object-contain' alt='image missing' />
           </Listbox.Button>
           {/* Transition for displaying the options */}
           <Transition
